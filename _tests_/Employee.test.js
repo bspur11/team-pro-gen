606c87e52fe120f4f49063622d34e4ddd8df6a05
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-13 15:22:40 -0500

    test fail

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,10 +1,22 @@
-const { tsMethodSignature, tsExternalModuleReference } = require('@babel/types');
-const { test } = require('@jest/globals');
+// const {
+//   tsMethodSignature,
+//   tsExternalModuleReference
+// } = require('@babel/types');
+const {
+  test,
+  expect
+} = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
-test('creates a employee object', () => {
-  const employee = new Employee('Dave');
+test('creates an employee object', () => {
+  const employee = new Employee('Bill');
+  expect(employee.name).toBe('Bill');
 
-  expect(employee.name).toBe('Dave');
- 
 });
+
+test('Checking for the id', () => {
+  const testValue = 50;
+  const employee = new Employee('steve', testValue);
+  expect(employee.id).tobe(testValue);
+});
+
