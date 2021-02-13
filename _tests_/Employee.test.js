0a53b7723a904455e63543a58e31ce7be133f1fe
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-13 17:37:45 -0500

    tests pass employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -17,6 +17,39 @@ test('creates an employee object', () => {
 test('Checking for the id', () => {
   const testValue = 50;
   const employee = new Employee('steve', testValue);
-  expect(employee.id).tobe(testValue);
+  expect(employee.id).toBe(testValue);
 });
+
+
+test('Checking for email', () => {
+  const testValue = "test@test.com";
+  const employee = new Employee('steve', 1, testValue);
+  expect(employee.email).toBe(testValue);
+});
+
+test('gets role of employee', () => {
+  const testValue = "Employee";
+  const employee = new Employee('steve', 1, testValue);
+  expect(employee.role).toBe(testValue);
+});
+
+test('getName', () => {
+  const testValue = 'Dave';
+  const employee =new Employee(testValue);
+  expect(employee.getName()).toBe(testValue);
+});
+
+test('to getId', () => {
+  const testValue = 50;
+  const employee = new Employee('Dave', testValue);
+  expect(employee.getId()).toBe(testValue);
+});
+
+test('to get the email', () => {
+  const testValue = "test@test.com";
+  const employee = new Employee('Dave', testValue);
+  expect(employee.getId()).toBe(testValue);
+});
+
+
 
