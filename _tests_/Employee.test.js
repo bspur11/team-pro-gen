d0b95e8305bafa0a8b3572550f0d0dc9b23d64f7
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-13 15:37:46 -0500

    latest fail

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -9,14 +9,14 @@ const {
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-  const employee = new Employee('Bill');
-  expect(employee.name).toBe('Bill');
+  const Employee = new Employee('Bill');
+  expect(Employee.name).toBe('Bill');
 
 });
 
 test('Checking for the id', () => {
   const testValue = 50;
-  const employee = new Employee('steve', testValue);
-  expect(employee.id).tobe(testValue);
+  const Employee = new Employee('steve', testValue);
+  expect(Employee.id).tobe(testValue);
 });
 
