fcb9fcfaf70f1d5c58b6d4a74d2eff2019631de2
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-13 21:23:10 -0500

    intern tests Pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -3,10 +3,22 @@ const {
   expect
 } = require('@jest/globals');
 
-const Intern = require("../lib/intern");
+const Intern = require("../lib/intern.js");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const Intern = new Intern("Joe", 1, "test@test.com", testValue);
-  expect(Intern.school).toBe(testValue);
+  const intern = new Intern("Joe", 1, "test@test.com", testValue);
+  expect(intern.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const intern = new Intern("Joe", 1, "test@test.com", "UCLA");
+  expect(intern.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
 });
